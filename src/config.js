b3fb7257f0e033e551e94e27e2cdbae04b0e628a
zhanfang <277208607@qq.com>
2016-05-15 22:44:48 +0800

    完成前端服务器配置升级

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
 			? 'http://api.jackhu.top/'
-			: 'http://localhost:8080/'
+			: '/'
 
 export const CookieDomain = (process.env.NODE_ENV === 'production')
 			? '.jackhu.top'
