082cfdf63e77d3c057e6d2601f211a6a4bb1d36f
zhanfang <fzhanxd@gmail.com>
2016-07-31 18:27:58 +0800

    修改api服务器端口号

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
 			? 'http://note.demozhan.com/'
-			: 'http://localhost:3344/'
+			: 'http://localhost:3345/'
 
 export const CookieDomain = (process.env.NODE_ENV === 'production')
 			? '.demozhan.com'
