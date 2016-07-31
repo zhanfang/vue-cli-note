22aac2453b89fe7e9d48d0a2e241c6320709043a
zhanfang <fzhanxd@gmail.com>
2016-07-31 19:43:08 +0800

    重新生成dist

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
-			? 'http://localhost:3345/'
+			? 'http://note.demozhan.com/'
 			: 'http://localhost:3345/'
 
 export const CookieDomain = (process.env.NODE_ENV === 'production')
